7d8e9dcba8deb37a731e2f26d291a45b294ff93a
cristian <ch374185@gmail.com>
2023-10-04 09:45:20 -0600

    01 Javascript asincrono

---
+++ asincrono.js
@@ -0,0 +1,18 @@
+const teams = ["Chelsie", "Bayern", "Cruz azul"]
+
+console.log(teams);
+
+function getTeams(){
+    return teams;
+}
+
+console.log('Console 2');
+console.log(getTeams());
+
+function asyncGetTeams(){
+    setTimeout(() => teams, 2000);
+}
+
+console.log('Console 3');
+console.log(asyncGetTeams());
+console.log('Termina script');
